f1cf17a4edaea53ccd230dbadb3314c432dd83b5
KapilSharma1306 <kapilsharmacoding1306202@gmail.com>
2025-08-11 15:36:49 +0530

    add a image in satsCounter component

--- src/components/StatsCounter.jsx
+++ src/components/StatsCounter.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import img from '../assets/statsCounter_imgs/img1.jpg'
 
 function StatsCounter() {
     const sectionRef = useRef();
@@ -49,17 +50,17 @@ function StatsCounter() {
     return (
         <section
             ref={sectionRef}
-            className="py-16 bg-blue-950 text-white text-center flex flex-col md:flex-row justify-between px-5 md:px-15 gap-5"
+            className="py-16 bg-blue-950 text-white text-center flex flex-col md:flex-row justify-between px-5 md:px-10 gap-5"
         >
             <div className="text-center">
                 <div className=" space-y-10 ">
                     <h1 className="font-bold text-4xl">Need the Best Help for Your Digital Growth ?</h1>
                     <p className="text-sm">Work With a Result-Driven No.1 AI Digital Marketing Agency. Contact Us Now and See the Difference!</p>
                 </div>
-                <button className="mt-10 text-[aqua] border cursor-pointer px-4 py-1 rounded-full border-[aqua]"><Link to={"/contact-us"}>Contact Us Now</Link></button>
+                <button className="mt-10 text-[aqua] border cursor-pointer px-4 py-1 rounded-full border-[aqua] hover:bg-blue-900 hover:scale-105 transition-all duration-300 "><Link to={"/contact-us"}>Contact Us Now</Link></button>
             </div>
 
-            <div className="grid grid-cols-2  gap-16 text-xl font-semibold mt-10 md:mt-0">
+            <div className="grid grid-cols-2  gap-16 text-xl font-semibold mt-10 md:ml-5 md:mt-0">
                 <div>
                     <span className="text-5xl text-[aqua]">{counts.clients}+</span><br />
                     <span className="text-sm text-gray-200">Happy Clients</span>
@@ -77,6 +78,10 @@ function StatsCounter() {
                     <span className="text-sm text-gray-200">Glorious Years</span>
                 </div>
             </div>
+            
+            <Link to={'https://www.designrush.com/'} target="blank" className="flex items-center justify-center md:ml-10 md:mt-0 mt-15">
+                <img src={img} alt="Logo" className="md:h-full md:w-[20rem] w-[60%] rounded-xl hover:scale-110 transition-all duration-500"/>
+            </Link>
         </section>
     );
 }
